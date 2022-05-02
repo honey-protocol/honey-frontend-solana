b7bfa78f38c3cc220cde126c161e25b5d10db408
BowTiedSwan <0xf85ce443D3F031728e0e97503C06322c2F51A0c7@ethmail.cc>
2022-05-02 13:45:30 +0300

    Add template component for main panel

---
+++ components/DashboardMainPanel.tsx
@@ -0,0 +1,18 @@
+import { Box, Button, Card, Spinner, Stack, Text, IconPlus } from 'degen';
+import React from 'react';
+
+
+const DashboardMainPanel = () => {
+  return (
+    <Box
+        backgroundColor="backgroundTertiary"
+        borderRadius="2xLarge"
+        width="full"
+        height='full'
+    >
+        <Text>1st panel</Text>
+    </Box>
+  );
+};
+
+export default DashboardMainPanel;
