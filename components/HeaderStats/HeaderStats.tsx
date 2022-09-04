5349124451e64c1aca6bface405c379e046ba5d7
daan <danieloshos3@gmail.com>
2022-09-04 09:31:40 +0100

    feat: header done

---
+++ components/HeaderStats/HeaderStats.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Progress, Space, Typography } from 'antd';
+import * as styles from './HeaderStats.css';
+import { vars } from 'styles/theme.css';
+import { BarPinItemIcon } from 'icons/BarPinItemIcon';
+const { Title, Paragraph, Text, Link } = Typography;
+
+const HeaderStats = () => {
+  return (
+    <Space size="middle" className={styles.headerStats}>
+      <Space align="end" size={2} direction="vertical">
+        <Text className={styles.number}>$ 1,250</Text>
+        <Text className={styles.caption}>Debt</Text>
+      </Space>
+      <div className={styles.statsContainer}>
+        <div className={styles.progressBarWrapperWithPin}>
+          <Progress
+            strokeWidth={5}
+            showInfo={false}
+            percent={100}
+            strokeColor={vars.colors.green}
+          />
+          <div className={styles.pinContainer}>
+            <BarPinItemIcon color={vars.colors.green} />
+          </div>
+        </div>
+        <Space>
+          <Text className={styles.number}>
+            20 %{' '}
+            <Text type="secondary" className={styles.caption}>
+              Risk
+            </Text>
+          </Text>
+        </Space>
+      </div>
+      <div className={styles.statsContainer} style={{ minWidth: '170px' }}>
+        <div className={styles.progressBarWrapperWithPin}>
+          <Progress
+            strokeWidth={5}
+            showInfo={false}
+            percent={100}
+            strokeColor="black"
+            className={styles.progressBarWithPin}
+          />
+          <div className={styles.pinContainer}>
+            <BarPinItemIcon color={'black'} />
+          </div>
+        </div>
+        <Text className={styles.number}>
+          $ 2,250{' '}
+          <Text type="secondary" className={styles.caption}>
+            Available
+          </Text>
+        </Text>
+      </div>
+
+      <div className={styles.statsContainer} style={{ minWidth: '50px' }}>
+        <Progress
+          strokeWidth={5}
+          showInfo={false}
+          percent={100}
+          strokeColor={vars.colors.grayMiddle}
+        />
+
+        <Text style={{ color: 'transparent' }}>{`.`}</Text>
+      </div>
+      <div className={styles.statsContainer} style={{ minWidth: '100px' }}>
+        <Progress
+          strokeWidth={5.3}
+          showInfo={false}
+          percent={100}
+          strokeColor={vars.colors.grayMiddle}
+        />
+        <Text type="secondary" className={styles.caption}>
+          Liquidation
+        </Text>
+      </div>
+      <Space size={2} direction="vertical">
+        <Text className={styles.number}>$ 5,000</Text>
+        <Text type="secondary" className={styles.caption}>
+          Deposit
+        </Text>
+      </Space>
+    </Space>
+  );
+};
+
+export default HeaderStats;
--- components/LayoutRedesign/LayoutRedesign.css.ts
+++ components/LayoutRedesign/LayoutRedesign.css.ts
@@ -1,7 +1,10 @@
 import { style, globalStyle } from '@vanilla-extract/css';
 import { vars } from 'styles/theme.css';
 
-export const layout = style({});
+export const layout = style({
+  padding: 20,
+  minHeight: '100vh'
+});
 
 globalStyle(`body ${layout} .ant-layout-header`, {
   backgroundColor: vars.colors.white
@@ -9,4 +12,14 @@ globalStyle(`body ${layout} .ant-layout-header`, {
 
 globalStyle(`body ${layout} .ant-layout-sider`, {
   backgroundColor: 'transparent'
+});
+
+export const layoutHeader = style({
+  height: 'unset',
+  position: 'relative',
+  borderRadius: vars.space.medium,
+  border: '2px solid black',
+  boxShadow: '4px 4px 0px 0px rgba(231, 180, 0, 1)',
+
+  zIndex: '1'
 });
