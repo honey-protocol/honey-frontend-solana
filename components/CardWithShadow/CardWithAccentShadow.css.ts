02a869165bfda1382e57ca9d463385e934abfb6c
daan <danieloshos3@gmail.com>
2022-09-05 09:22:08 +0100

    fix: colors fix

--- components/CardWithShadow/CardWithAccentShadow.css.ts
+++ components/CardWithShadow/CardWithAccentShadow.css.ts
@@ -4,7 +4,7 @@ import { vars } from 'styles/theme.css';
 export const card = style({
   overflow: 'hidden',
   borderRadius: vars.space.medium,
-  border: '2px solid black',
-  boxShadow: '4px 4px 0px 0px rgba(231, 180, 0, 1)',
+  border: `2px solid ${vars.colors.black}`,
+  boxShadow: `4px 4px 0px 0px ${vars.colors.brownLight}`,
   marginBottom: '5px'
 });
