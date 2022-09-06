c37853b8ae76d73d302def62e9d617cedb9291a8
Egor Bukraba <egor@humandone.com>
2022-09-06 11:17:13 +0400

    feat(markets): add table sorting

--- styles/markets.css.ts
+++ styles/markets.css.ts
@@ -1,4 +1,4 @@
-import { globalStyle, style } from '@vanilla-extract/css';
+import { globalStyle, style, styleVariants } from '@vanilla-extract/css';
 import { typography, vars } from './theme.css';
 
 export const marketsPage = style({});
@@ -64,3 +64,66 @@ export const toggleText = style([
     marginLeft: 12
   }
 ]);
+
+const headerCellBasic = style([
+  typography.button,
+  {
+    display: 'flex',
+    alignItems: 'center',
+    textTransform: 'uppercase',
+    color: vars.colors.black
+  }
+]);
+export const headerCell = styleVariants({
+  active: [
+    headerCellBasic,
+    {
+      color: vars.colors.brownMiddle
+    }
+  ],
+  disabled: [
+    headerCellBasic,
+    {
+      color: vars.colors.black
+    }
+  ]
+});
+
+const sortIconBasic = style({
+  width: 20,
+  height: 20,
+  marginLeft: 4
+});
+export const sortIcon = styleVariants({
+  disabled: [
+    sortIconBasic,
+    {
+      background: 'url(/images/sorter.svg) center no-repeat'
+    }
+  ],
+  ascend: [
+    sortIconBasic,
+    {
+      background: 'url(/images/sorter-active.svg) center no-repeat',
+      transform: 'rotate(180deg)'
+    }
+  ],
+  descend: [
+    sortIconBasic,
+    {
+      background: 'url(/images/sorter-active.svg) center no-repeat'
+    }
+  ]
+});
+export const table = style({});
+const tableChildSelector = (selector: string) => {
+  return `${table} ${selector}`;
+};
+const tcs = tableChildSelector;
+
+// globalStyle(tcs(`.ant-table-container > .ant-table-content > table > .ant-table-thead > tr > .ant-table-column-has-sorters > .ant-table-column-sorters > .ant-table-column-sorter`), {
+//   display: "none"
+// })
+globalStyle(tcs(`.ant-table-column-sorter`), {
+  display: 'none'
+});
