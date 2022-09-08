d608d4bea4f392adde52e9b8c5eea501184984b6
daan <danieloshos3@gmail.com>
2022-09-08 03:19:37 +0100

    empty state details moved to components

---
+++ components/EmptyStateDetails/EmptyStateDetails.css.ts
@@ -0,0 +1,31 @@
+import { style } from '@vanilla-extract/css';
+import { typography } from 'styles/theme.css';
+
+export const emptyStateContent = style({
+  width: '100%',
+  maxWidth: '300px',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'center'
+});
+
+export const emptyStateTitle = style([
+  typography.body,
+  {
+    marginTop: 10,
+    marginBottom: 4
+  }
+]);
+
+export const emptyStateDescription = style([
+  typography.description,
+  {
+    fontWeight: '500'
+  }
+]);
+
+export const emptyStateWalletBtn = style({
+  marginTop: 12
+});
