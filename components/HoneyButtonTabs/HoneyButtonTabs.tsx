d3e992bf8d00251af1e6617759ef1c47e9bdd19a
Mihail Osipov <m.osipov@humandone.com>
2022-09-14 10:46:33 +0000

    feat(liquidation): add expandable table and mocks

---
+++ components/HoneyButtonTabs/HoneyButtonTabs.tsx
@@ -0,0 +1,39 @@
+import React, { FC } from 'react';
+import * as style from './HoneyButtonTabs.css';
+
+export interface HoneyButtonTabsProps {
+  items: HoneyButtonTabsItem[];
+  activeItemSlug: string;
+  onClick: (itemSlug: string) => void;
+}
+
+type HoneyButtonTabsItem = {
+  name: string;
+  slug: string;
+};
+
+export const HoneyButtonTabs: FC<HoneyButtonTabsProps> = ({
+  items,
+  activeItemSlug,
+  onClick
+}) => (
+  <div className={style.periodSelector}>
+    <div className={style.periodSelectorContent}>
+      {items.map((item, index) => {
+        const itemStatus = item.slug == activeItemSlug ? 'active' : 'passive';
+        const isVisibleDivider =
+          items.length - 1 !== index && itemStatus !== 'active';
+        return (
+          <div
+            key={item.slug}
+            className={style.periodSelectorItem[itemStatus]}
+            onClick={() => onClick(item.slug)}
+          >
+            {item.name}
+            {isVisibleDivider && <div className={style.verticalDivider} />}
+          </div>
+        );
+      })}
+    </div>
+  </div>
+);
