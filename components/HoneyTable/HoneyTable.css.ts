0b987d8ece7dbd206aebc076806b7f170e2e6ab7
daan <danieloshos3@gmail.com>
2022-09-15 07:14:47 +0100

    remove table head vertical padding

--- components/HoneyTable/HoneyTable.css.ts
+++ components/HoneyTable/HoneyTable.css.ts
@@ -30,6 +30,10 @@ globalStyle(
   }
 );
 
+globalStyle(`${bcs('.ant-table-thead > tr > th')}`, {
+  padding: '0 16px'
+});
+
 globalStyle(`${bcs('.ant-table table')}`, {
   borderCollapse: 'separate',
   borderSpacing: '0 8px'
