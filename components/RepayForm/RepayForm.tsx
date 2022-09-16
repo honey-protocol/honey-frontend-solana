8f9e6bbef796636be1d0e88abf8ddbbacee012af
daan <danieloshos3@gmail.com>
2022-09-16 10:03:57 +0100

    use tooltip in repay form

--- components/RepayForm/RepayForm.tsx
+++ components/RepayForm/RepayForm.tsx
@@ -170,6 +170,7 @@ const RepayForm = (props: RepayProps) => {
               value={fu(nftPrice)}
               valueSize="big"
               footer={<span>Estimated value</span>}
+              toolTipLabel="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
             />
           </div>
           <div className={styles.col}>
@@ -177,19 +178,25 @@ const RepayForm = (props: RepayProps) => {
               value={fp(75)}
               valueSize="big"
               footer={<span>Liquidation at</span>}
+              toolTipLabel="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
             />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.col}>
-            <InfoBlock title={'Risk level'} value={fu(loanToValue)} />
+            <InfoBlock
+              title={'Risk level'}
+              value={fu(loanToValue)}
+              toolTipLabel="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
+            />
           </div>
           <div className={styles.col}>
             <InfoBlock
               title={'New risk level'}
               value={fu(0)}
               isDisabled={true}
+              toolTipLabel="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has "
             />
           </div>
         </div>
