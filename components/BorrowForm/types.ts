ff830800321c38b6bf0f79f711300fbac5f81e95
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-09-19 11:28:50 +0200

    add types

--- components/BorrowForm/types.ts
+++ components/BorrowForm/types.ts
@@ -2,5 +2,7 @@ export type BorrowProps = {
     collectionId?: string;
     availableNFTs?: any;
     openPositions?: any;
+    nftPrice?: number;
+    executeDepositNFT: (mint: any) => void;
   };
   
