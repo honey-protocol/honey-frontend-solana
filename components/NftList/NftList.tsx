cd57692c3ab3dc7623d22ef13c5f8c46cba1dd15
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-09-19 11:34:13 +0200

    render dynamic fields | value nft and allowance

--- components/NftList/NftList.tsx
+++ components/NftList/NftList.tsx
@@ -1,3 +1,5 @@
+import { MAX_LTV } from 'constants/loan';
+import { RoundHalfDown } from 'helpers/utils';
 import React from 'react';
 import NftCard from '../NftCard/NftCard';
 import { NftCardProps } from '../NftCard/types';
@@ -5,14 +7,16 @@ import * as style from './NftList.css';
 
 type NftListProps = {
   data: NftCardProps[];
-  selectNFT: (name: string, id: string, img: string) => void;
+  selectNFT: (name: string, id: string, img: string, mint: any) => void;
+  nftPrice: any;
 };
 
 const NftList = (props: NftListProps) => {
-  const { data, selectNFT } = props;
+  const { data, selectNFT, nftPrice } = props;
   
   function handleClick(item: any) {
-    selectNFT(item.name, item.tokenId, item.image);
+    console.log('item', item)
+    selectNFT(item.name, item.tokenId, item.image, item.mint);
   }
 
   return (
@@ -25,6 +29,8 @@ const NftList = (props: NftListProps) => {
                 onClick={() => handleClick(item)}
                 {...item}
                 hasBorder={index !== data.length - 1}
+                text={`$ ${nftPrice.toFixed(2)} value`}
+                buttonText={(RoundHalfDown(nftPrice * MAX_LTV, 4)).toString()}
               />
             </div>
           )
