09383ec8b550aed497904fbad609afbca15ae84c
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-09-21 16:31:54 +0200

    add types

--- components/RepayForm/types.ts
+++ components/RepayForm/types.ts
@@ -2,6 +2,10 @@ export type RepayProps = {
     collectionId?: string;
     openPositions?: any;
     nftPrice?: number;
+    userAllowance: number;
+    userDebt: number;
+    userUSDCBalance: number;
+    loanToValue: number;
     executeWithdrawNFT: (mint: any) => void;
     executeRepay: (val: any) => void;
   };
