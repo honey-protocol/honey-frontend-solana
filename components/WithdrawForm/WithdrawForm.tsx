de9508bc941aefe7967226110871a238092d4cea
daan <danieloshos3@gmail.com>
2022-09-22 16:19:58 +0100

    add tooltip for APR

--- components/WithdrawForm/WithdrawForm.tsx
+++ components/WithdrawForm/WithdrawForm.tsx
@@ -104,7 +104,12 @@ const WithdrawForm = (props: WithdrawFormProps) => {
             <InfoBlock
               value={fp(20)}
               valueSize="big"
-              footer={<span>Estimated APR</span>}
+              toolTipLabel="APY is measured by compounding the weekly interest rate"
+              footer={
+                <span className={hAlign}>
+                  Estimated APR <div className={questionIcon} />
+                </span>
+              }
             />
           </div>
           <div className={styles.col}>
