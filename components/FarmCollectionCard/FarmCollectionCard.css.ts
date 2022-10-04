42c55922d999601a80c47f2de69f37686d7e8e50
daan <danieloshos3@gmail.com>
2022-10-04 10:10:20 +0100

    fix  hover border

--- components/FarmCollectionCard/FarmCollectionCard.css.ts
+++ components/FarmCollectionCard/FarmCollectionCard.css.ts
@@ -1,13 +1,14 @@
 import { style } from '@vanilla-extract/css';
-import { vars } from 'degen';
+import { vars } from 'styles/theme.css';
+
 export const cardContainer = style({
   transition: 'all .3s',
   boxSizing: 'border-box',
   borderWidth: 2,
   borderColor: 'black',
   ':hover': {
-    borderColor: vars.colors.yellow,
-    borderWidth: 5
+    borderColor: vars.colors.brownLight,
+    borderWidth: 3
   }
 });
 
