7e720b0863f2349ab390460798dc940371c2bf65
BowTiedHeron <bowtiedheron@protonmail.com>
2022-10-08 11:26:10 +0200

    update constants || MAX_LTV && OPTIMAL_RATIO_ONE

--- constants/interestRate.ts
+++ constants/interestRate.ts
@@ -1,6 +1,6 @@
-export const OPTIMAL_RATIO_ONE = .4;
-export const OPTIMAL_RATIO_TWO = .8;
-export const BASE_BORROW_RATE = .1;
-export const BORROW_RATE_ONE = .25;
-export const BORROW_RATE_TWO = .4;
+export const OPTIMAL_RATIO_ONE = 0.5;
+export const OPTIMAL_RATIO_TWO = 0.8;
+export const BASE_BORROW_RATE = 0.1;
+export const BORROW_RATE_ONE = 0.25;
+export const BORROW_RATE_TWO = 0.4;
 export const BORROW_RATE_THREE = 1.4;
