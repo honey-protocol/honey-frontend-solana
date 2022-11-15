5cc67ef4f8f60ae54660c01b26acddb039149d32
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-11-15 10:13:48 +0100

    interestrate wip

--- constants/interestRate.ts
+++ constants/interestRate.ts
@@ -1,5 +1,6 @@
-export const OPTIMAL_RATIO_ONE = 0.5;
+export const OPTIMAL_RATIO_ONE = 0.4;
 export const OPTIMAL_RATIO_TWO = 0.8;
+export const OPTIMAL_RATIO_THREE = 1.6;
 export const BASE_BORROW_RATE = 0.1;
 export const BORROW_RATE_ONE = 0.25;
 export const BORROW_RATE_TWO = 0.4;
