5dd24284a35a825bc119e6fd144c02ff54efa872
Bowtiedpyro <bowtiedpyro@gmail.com>
2022-11-23 13:18:25 +0100

    add comments

--- components/BidForm/BidForm.tsx
+++ components/BidForm/BidForm.tsx
@@ -39,13 +39,14 @@ const BidForm = (props: BidFormProps) => {
     handlePlaceBid,
     onCancel
   } = props;
+  // state declarations
   const [valueUSD, setValueUSD] = useState<number>(0);
   const [valueSOL, setValueSOL] = useState<number>(0);
   const [sliderValue, setSliderValue] = useState(0);
   const [userBidValue, setUserBidValue] = useState(0);
-
+  // import toast for responses
   const { toast, ToastComponent } = useToast();
-
+  // set constants
   const maxValue = 1000;
   const solPrice = fetchedSolPrice;
   // Put your validators here
