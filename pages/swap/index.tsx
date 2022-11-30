fbede44cb55433bbdabf6727c13885dae9643d21
daan <danieloshos3@gmail.com>
2022-11-30 10:08:31 +0100

    add featured tokens to output token search modal

--- pages/swap/index.tsx
+++ pages/swap/index.tsx
@@ -567,6 +567,16 @@ const Swap: NextPage = () => {
                 setIsOutputTokenModalVisible(false);
               }}
               tokens={tokensDetails}
+              featuredTokens={[
+                'HonyeYAaTPgKUgQpayL914P6VAqbQZPrbkGMETZvW4iN', // $HONEY
+                'EchesyfXePKdLtoiZSL8pBe8Myagyy8ZRqsACNCFGnvp', // FIDA
+                'So11111111111111111111111111111111111111112', // wSOL
+                '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R', // RAY
+                'MangoCzJ36AjZyKwVj3VnYU4GTonjfVEnJmvvWaxLac', // Mango
+                'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB', // USDT
+                'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v', // USDC
+                'mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So' // mSOL
+              ]}
               visible={isOutputTokenModalVisible}
               onCancel={() => setIsOutputTokenModalVisible(false)}
               destroyOnClose
