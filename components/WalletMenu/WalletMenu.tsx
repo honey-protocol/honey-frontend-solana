c0d73e521ec1b23a78384d2aa10c779443e5fb8b
BowTiedHeron <bowtiedheron@protonmail.com>
2022-12-13 12:23:41 +0100

    add back feature flag

--- components/WalletMenu/WalletMenu.tsx
+++ components/WalletMenu/WalletMenu.tsx
@@ -41,7 +41,7 @@ const WalletMenu = () => {
   ) : (
     <div className={styles.walletDropdownWrapper}>
       <div className={styles.dialectIconWrapper}>
-        <DialectNotifications />
+        {featureFlags.isDialectNotificationsEnabled && <DialectNotifications />}
       </div>
       <Dropdown overlay={menu}>
         <a onClick={e => e.preventDefault()}>
