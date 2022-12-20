1be2eeb2efaecc76ac18a6ec19d4d7849cb5e427
BowTiedHeron <93859808+BowTiedHeron@users.noreply.github.com>
2022-12-20 13:07:12 +0100

    feat(Dialect): styling update for dialect integration (#233)
    * dialect form styles 1
    
    * notification indication dot style
    
    * comments, date to left and btn padding
    
    Co-authored-by: daan <danieloshos3@gmail.com>

--- components/DialectNotifications/DialectNotifications.css.tsx
+++ components/DialectNotifications/DialectNotifications.css.tsx
@@ -1,21 +1,94 @@
 import { globalStyle, style } from '@vanilla-extract/css';
-import { typography, vars } from '../../styles/theme.css';
+import { breakpoints, typography, vars } from '../../styles/theme.css';
 
 export const dialectNotification = style({
   width: 195,
   height: 20
 });
 
+// indicator dot style
+globalStyle(
+  `${dialectNotification} .dialect .dt-absolute.dt-h-3.dt-w-3.dt-z-50.dt-rounded-full.dt-bg-accent.dt-text-white`,
+  {
+    background: vars.colors.brownLight,
+    left: 12
+  }
+);
+
 const popoverSelector = `${dialectNotification} .dialect .dialect > .dt-fixed`;
 
-globalStyle(`${popoverSelector}`, {});
+globalStyle(`${popoverSelector}`, {
+  maxWidth: 360,
+  height: 'calc(100vh - 120px)',
+  '@media': {
+    [`screen and (max-width: ${breakpoints.mobile}px)`]: {
+      maxWidth: 'unset',
+      height: '100vh'
+    }
+  }
+});
 
 globalStyle(`${popoverSelector} .dt-flex-row`, {
-  padding: '16px 16px 16px 16px',
+  padding: '14px 14px 16px 16px',
   border: 'none',
   maxHeight: 'initial',
-  minHeight: 0,
-  marginBottom: `-16px`
+  minHeight: 0
+});
+
+globalStyle(`${popoverSelector} *`, {
+  fontFamily: 'Scandia !important',
+  fontWeight: '500',
+  fontSize: '14px',
+  lineHeight: '20px'
+});
+
+//date text style
+globalStyle(`${popoverSelector} .dt-text-right > p`, {
+  fontWeight: '400',
+  fontSize: '12px',
+  lineHeight: '16px',
+  textAlign: 'left'
+});
+
+globalStyle(`${popoverSelector} label`, {
+  ...typography.caption
+});
+
+globalStyle(`${popoverSelector} input`, {
+  border: 'none'
+});
+
+globalStyle(`${popoverSelector} button`, {
+  ...typography.button,
+  fontFamily: 'Red Hat Mono !important',
+  borderRadius: '8px',
+  height: 'unset',
+  display: 'flex',
+  // display: 'inline-flex',
+  padding: '0 !important',
+  border: 'none',
+  gap: '10px',
+  transition: 'all .5s',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: vars.colors.brownLight,
+  color: vars.colors.black
+});
+
+globalStyle(`${popoverSelector} button:not(:has(svg))`, {
+  padding: '7px !important'
+});
+
+globalStyle(`${popoverSelector} button svg`, {
+  width: 40,
+  height: 'auto',
+  background: vars.colors.white,
+  borderRadius: 8,
+  padding: 7
+});
+
+globalStyle(`${popoverSelector} input[type="checkbox"] + span`, {
+  background: vars.colors.secondaryBrownMiddle
 });
 
 globalStyle(`${popoverSelector} .dt-flex-row > .dt-text-base`, {
