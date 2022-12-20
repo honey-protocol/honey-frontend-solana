6b8d4b3bbaac5691dbe0f3d8070b49a617aedd9b
daan <danieloshos3@gmail.com>
2022-12-20 10:41:02 +0100

    dialect form styles 1

--- components/DialectNotifications/DialectNotifications.css.tsx
+++ components/DialectNotifications/DialectNotifications.css.tsx
@@ -1,5 +1,5 @@
 import { globalStyle, style } from '@vanilla-extract/css';
-import { typography, vars } from '../../styles/theme.css';
+import { breakpoints, typography, vars } from '../../styles/theme.css';
 
 export const dialectNotification = style({
   width: 195,
@@ -8,14 +8,72 @@ export const dialectNotification = style({
 
 const popoverSelector = `${dialectNotification} .dialect .dialect > .dt-fixed`;
 
-globalStyle(`${popoverSelector}`, {});
+globalStyle(`${popoverSelector}`, {
+  maxWidth: 360,
+  height: 'calc(100vh - 120px)',
+  '@media': {
+    [`screen and (max-width: ${breakpoints.mobile}px)`]: {
+      maxWidth: 'unset',
+      height: '100vh'
+    }
+  }
+});
 
 globalStyle(`${popoverSelector} .dt-flex-row`, {
-  padding: '16px 16px 16px 16px',
+  padding: '14px 14px 16px 16px',
   border: 'none',
   maxHeight: 'initial',
-  minHeight: 0,
-  marginBottom: `-16px`
+  minHeight: 0
+});
+
+globalStyle(`${popoverSelector} *`, {
+  fontFamily: 'Scandia !important',
+  fontWeight: '500',
+  fontSize: '14px',
+  lineHeight: '20px'
+});
+
+globalStyle(`${popoverSelector} .dt-text-right > p`, {
+  fontWeight: '400',
+  fontSize: '12px',
+  lineHeight: '16px'
+});
+
+globalStyle(`${popoverSelector} label`, {
+  ...typography.caption
+});
+
+globalStyle(`${popoverSelector} input`, {
+  border: 'none'
+});
+
+globalStyle(`${popoverSelector} button`, {
+  ...typography.button,
+  fontFamily: 'Red Hat Mono !important',
+  borderRadius: '8px',
+  height: 'unset',
+  display: 'flex',
+  // display: 'inline-flex',
+  padding: '0 !important',
+  border: 'none',
+  gap: '10px',
+  transition: 'all .5s',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: vars.colors.brownLight,
+  color: vars.colors.black
+});
+
+globalStyle(`${popoverSelector} button svg`, {
+  width: 40,
+  height: 'auto',
+  background: vars.colors.white,
+  borderRadius: 8,
+  padding: 7
+});
+
+globalStyle(`${popoverSelector} input[type="checkbox"] + span`, {
+  background: vars.colors.secondaryBrownMiddle
 });
 
 globalStyle(`${popoverSelector} .dt-flex-row > .dt-text-base`, {
