3e9047c7ce7eb1ea449b56f91c9b78b3ac5e9643
daan <danieloshos3@gmail.com>
2022-12-24 14:10:24 +0100

    new theme variables

--- styles/theme.css.ts
+++ styles/theme.css.ts
@@ -1,9 +1,73 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
+import {
+  createGlobalTheme,
+  createGlobalThemeContract
+} from '@vanilla-extract/css';
 import { defineProperties, createSprinkles } from '@vanilla-extract/sprinkles';
 import { Property } from 'csstype';
 import TextTransform = Property.TextTransform;
 
-export const vars = createGlobalTheme(':root', {
+export const vars = createGlobalThemeContract({
+  colors: {
+    brownLight: 'brownLight',
+    brownMiddle: 'brownMiddle',
+    brownDark: 'brownDark',
+
+    secondaryBrownLight: 'secondaryBrownLight',
+    secondaryBrownMiddle: 'secondaryBrownMiddle',
+
+    grayLight: 'rywre',
+    grayMiddle: 'rytrd',
+    grayDark: 'drg',
+
+    green: 'jkh',
+    greenDark: 'greenDark',
+    greenDarkest: 'greenDarkest',
+
+    blue: 'hkjhlkjh',
+
+    red: 'bkj',
+    redDark: 'redDark',
+    lightRedTransparent: 'lightRedTransparent',
+
+    grayTransparent: 'grayTransparent',
+    lightGrayTransparent: 'lightGrayTransparent',
+    whiteTransparent: 'whiteTransparent',
+
+    white: 'bhhioyigj',
+    black: 'fdyjfghk',
+
+    background: 'background',
+    foreground: 'foreground',
+
+    text: 'primary',
+    textSecondary: 'secondary',
+    textTertiary: 'tertiary',
+
+    borderPrimary: 'primary-border',
+    borderSecondary: 'secondary-border'
+  },
+  space: {
+    none: 'adsj',
+    small: 'fs',
+    medium: 'q',
+    large: 'fadsj'
+  },
+  width: {
+    ['1/2']: 'kls',
+    full: 'adsf'
+  },
+  shadow: {
+    yellow: {
+      boxShadow: `asdf`
+    }
+  }
+});
+
+const grayLight = '#F5F5F5';
+const white = '#FFFFFF';
+const black = '#000000';
+const darkForeground = '#141414';
+createGlobalTheme('.honey-light-theme', vars, {
   colors: {
     brownLight: '#E7B400',
     brownMiddle: '#BE9200',
@@ -12,7 +76,7 @@ export const vars = createGlobalTheme(':root', {
     secondaryBrownLight: '#FBF3D6',
     secondaryBrownMiddle: '#ECDFAF',
 
-    grayLight: '#F5F5F5',
+    grayLight,
     grayMiddle: '#E5E5E5',
     grayDark: '#D9D9D9',
 
@@ -30,8 +94,69 @@ export const vars = createGlobalTheme(':root', {
     lightGrayTransparent: 'rgba(0, 0, 0, 0.12)',
     whiteTransparent: 'rgba(255, 255, 255, 0.7)',
 
-    white: '#FFFFFF',
-    black: '#111111'
+    white,
+    black: '#111111',
+    foreground: white,
+    background: grayLight,
+    text: black,
+    textSecondary: 'rgba(0, 0, 0, 0.85)',
+    textTertiary: 'rgba(0, 0, 0, 0.4)',
+    borderPrimary: black,
+    borderSecondary: 'rgba(0, 0, 0, 0.12)'
+  },
+  space: {
+    none: '0',
+    small: '8px',
+    medium: '16px',
+    large: '32px'
+  },
+  width: {
+    ['1/2']: '50%',
+    full: '100%'
+  },
+  shadow: {
+    yellow: {
+      boxShadow: `4px 4px 0px 0px #FBF3D6`
+    }
+  }
+});
+
+createGlobalTheme('.honey-dark-theme', vars, {
+  colors: {
+    brownLight: '#E7B400',
+    brownMiddle: '#BE9200',
+    brownDark: '#AA8300',
+
+    secondaryBrownLight: '#524B32',
+    secondaryBrownMiddle: '#ECDFAF',
+
+    grayLight,
+    grayMiddle: '#3A3A3A',
+    grayDark: '#D9D9D9',
+
+    green: '#02AF00',
+    greenDark: '#007700',
+    greenDarkest: '#008800',
+
+    blue: '#009EE7',
+
+    red: '#CF4536',
+    redDark: '#A62B1E',
+    lightRedTransparent: 'rgba(166, 43, 30, 0.1)',
+
+    grayTransparent: 'rgba(17, 17, 17, 0.4)',
+    lightGrayTransparent: 'rgba(0, 0, 0, 0.12)',
+    whiteTransparent: 'rgba(255, 255, 255, 0.7)',
+
+    white,
+    black,
+    foreground: darkForeground,
+    background: black,
+    text: white,
+    textSecondary: 'rgba(255, 255, 255, 0.85)',
+    textTertiary: 'rgba(255, 255, 255, 0.4)',
+    borderPrimary: grayLight,
+    borderSecondary: 'rgba(255, 255, 255, 0.5)'
   },
   space: {
     none: '0',
@@ -55,7 +180,8 @@ export const typography = {
     fontFamily: 'Scandia',
     fontWeight: '500',
     fontSize: '32px',
-    lineHeight: '38px'
+    lineHeight: '38px',
+    color: vars.colors.text
   },
   title: {
     fontFamily: 'Scandia',
