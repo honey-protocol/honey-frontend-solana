135600096f1ccaa0358919886d61ed6a20e4dff0
BowTiedHeron <93859808+BowTiedHeron@users.noreply.github.com>
2023-01-18 23:00:06 +0100

    remove log in dialect component

--- components/DialectNotifications/DialectNotifications.tsx
+++ components/DialectNotifications/DialectNotifications.tsx
@@ -7,7 +7,7 @@ import * as styles from './DialectNotifications.css';
 const DIALECT_DAPP_ADDRESS = process.env.NEXT_PUBLIC_DIALECT_DAPP_ADDRESS;
 
 export const DialectNotifications = () => {
-  console.log('DIALECT_DAPP_ADDRESS', DIALECT_DAPP_ADDRESS)
+  // console.log('DIALECT_DAPP_ADDRESS', DIALECT_DAPP_ADDRESS)
   return DIALECT_DAPP_ADDRESS ? (
     <div className={styles.dialectNotification}>
       <NotificationsButton
