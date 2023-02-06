437ff46e3439023e1ca15bffa852fb9c3d8b7b69
Bowtiedpyro <bowtiedpyro@gmail.com>
2023-02-06 18:33:33 +0100

    add type for props

---
+++ pages/borrow/types.ts
@@ -0,0 +1,6 @@
+import { HoneyClient, HoneyMarket, HoneyReserve, HoneyUser } from "@honey-finance/sdk";
+import { PublicKey } from "@solana/web3.js";
+
+export type BorrowProps = {
+  ssrMarketData: any
+};
