c63a74718ff8f42d54f6d60430002457cb4223aa
Brian Broeking <bbroeking456@gmail.com>
2023-03-29 14:39:34 -0500

    Update index.tsx

--- helpers/marketHelpers/index.tsx
+++ helpers/marketHelpers/index.tsx
@@ -340,7 +340,9 @@ export const BORROW_FEE = 0.015; // Flagging min interest rate as fee
 
 // Honey Program ID
 export const HONEY_GENESIS_MARKET_ID =
-  'ACv7BxtW7QzvFyLtmxtB8xZBQrfHAyTAjdCZ3q4QEowF';
+  '6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3';
+// export const HONEY_GENESIS_MARKET_ID =
+//   'ACv7BxtW7QzvFyLtmxtB8xZBQrfHAyTAjdCZ3q4QEowF';
 export const HONEY_GENESIS_BEE_MARKET_NAME = 'Honey Genesis Bee';
 // swap to TEST_HONEY_PROGRAM_ID for test program on mainnet, for development of new features
 export const HONEY_PROGRAM_ID = 'hNEYyRsRBVq2La65V1KjvdbTE39w36gwrdjkmcpvysk';
