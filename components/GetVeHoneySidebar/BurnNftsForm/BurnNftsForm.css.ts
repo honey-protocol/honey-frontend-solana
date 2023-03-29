72630e151466837f05898b8b424d491c1c5b46de
Brian Broeking <bbroeking456@gmail.com>
2023-03-29 01:19:43 -0500

    Update BurnNftsForm.css.ts

--- components/GetVeHoneySidebar/BurnNftsForm/BurnNftsForm.css.ts
+++ components/GetVeHoneySidebar/BurnNftsForm/BurnNftsForm.css.ts
@@ -71,3 +71,48 @@ export const list = style({
   display: 'flex',
   flexDirection: 'column'
 });
+
+export const listItem = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: '14px 0',
+  alignItems: 'center'
+});
+
+export const listItemLeft = style({
+  display: 'flex'
+});
+export const listItemIcon = style({
+  width: 34,
+  height: 34,
+  flexShrink: 0,
+  marginRight: 15
+});
+export const itemCollection = style({
+  display: 'flex',
+  flexDirection: 'column'
+});
+export const itemCollectionName = style([
+  typography.caption,
+  {
+    color: vars.colors.textTertiary,
+    marginBottom: 2
+  }
+]);
+export const itemCollectionValue = style({
+  display: 'flex',
+  alignItems: 'flex-end'
+});
+export const itemCollectionValueCount = style([
+  typography.numbersRegular,
+  {
+    color: vars.colors.text,
+    marginRight: 4
+  }
+]);
+export const itemCollectionToken = style([
+  typography.caption,
+  {
+    color: vars.colors.text
+  }
+]);
