f70be5d8d1c2476b4dbfc009d5d73f0b0362ead2
daan <danieloshos3@gmail.com>
2023-05-12 11:50:16 +0100

    add usdc market and adjust market data type

--- helpers/marketHelpers/index.tsx
+++ helpers/marketHelpers/index.tsx
@@ -38,6 +38,7 @@ export const marketCollections: MarketTableRow[] = [
     key: 'HNYG',
     name: 'Honey Genesis Bee',
     verifiedCreator: '6vRx1iVZo3xfrBHdpvuwArL2jucVj9j9nLpd2VUTTGMG',
+    loanCurrency: 'SOL',
     rate: 0,
     available: 0,
     value: 0,
@@ -53,6 +54,36 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Honey Genesis Bee',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://dl.airtable.com/.attachmentThumbnails/6b6c8954aed777a74de52fd70f8751ab/46b325db',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
+      discountedMarket: false
+    }
+  },
+  {
+    id: 'FMTyFFxYQdttS93ti7qGRMtVTdqUSPdEk5qkMRqE9qXu',
+    key: 'HBG',
+    name: 'Honey Genesis Bee',
+    verifiedCreator: '6vRx1iVZo3xfrBHdpvuwArL2jucVj9j9nLpd2VUTTGMG',
+    loanCurrency: 'USDC',
+    rate: 0,
+    available: 0,
+    value: 0,
+    allowance: 0,
+    connection: undefined,
+    user: undefined,
+    debt: 0,
+    utilizationRate: 0,
+    openPositions: [],
+    constants: {
+      marketId: 'FMTyFFxYQdttS93ti7qGRMtVTdqUSPdEk5qkMRqE9qXu',
+      verifiedCreator: '6vRx1iVZo3xfrBHdpvuwArL2jucVj9j9nLpd2VUTTGMG',
+      marketName: 'Honey Genesis Bee',
+      marketImage:
+        'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://dl.airtable.com/.attachmentThumbnails/6b6c8954aed777a74de52fd70f8751ab/46b325db',
+      marketLoanCurrency: 'USDC',
+      marketLoanCurrencyImage: '/images/USDC.svg',
+      marketLoanCurrencyTokenMintAddress:
+        'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v',
       discountedMarket: false
     }
   },
@@ -61,6 +92,7 @@ export const marketCollections: MarketTableRow[] = [
     key: 'NOOT',
     name: 'Pesky Penguin',
     verifiedCreator: 'A6XTVFiwGVsG6b6LsvQTGnV5LH3Pfa3qW3TGz8RjToLp',
+    loanCurrency: 'SOL',
     rate: 0,
     available: 0,
     value: 0,
@@ -76,6 +108,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Pesky Penguin',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://i.imgur.com/37nsjBZ.png',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -84,6 +118,7 @@ export const marketCollections: MarketTableRow[] = [
     key: 'LIFINITY',
     name: 'LIFINITY Flares',
     verifiedCreator: 'EWyWxSkxWHWGzxfCd9kG7zGrKTUDbZGiV6VbFJF8YfqN',
+    loanCurrency: 'SOL',
     rate: 0,
     available: 0,
     value: 0,
@@ -99,6 +134,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'LIFINITY Flares',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://dl.airtable.com/.attachmentThumbnails/6972d5c2efb77d49be97b07ccf4fbc69/e9572fb8',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: true
     }
   },
@@ -107,6 +144,7 @@ export const marketCollections: MarketTableRow[] = [
     key: 'ATD',
     name: 'OG Atadians',
     verifiedCreator: '4pi2MRDQgGVFwV1Hv8ka7hSSu9TgdFmafk6jtQhrB9HN',
+    loanCurrency: 'SOL',
     rate: 0,
     available: 0,
     value: 0,
@@ -122,6 +160,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'OG Atadians',
       marketImage:
         'https://i.seadn.io/gae/O8GK9vCJ5Cdtjz9aP3x7M4Bc4Z5HNgcsP0AcZFRjhC8p8fU4icgv2ecfNfZhMBjcYycVgJ2gpvCae_1TfXdbGOg2d6pKCdXRPlSsfQ?auto=format&w=384',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: true
     }
   },
@@ -130,6 +170,7 @@ export const marketCollections: MarketTableRow[] = [
     key: 'Ukiyo',
     name: 'Ukiyo',
     verifiedCreator: '9dQhwT67yaTk3YKhTN7f4jKFEAYtNtMotyVmVswmAQKe',
+    loanCurrency: 'SOL',
     rate: 0,
     available: 0,
     value: 0,
@@ -145,6 +186,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Ukiyo',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/ukiyo_pfp_1663178207810.jpeg',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -154,6 +197,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Elixir: Ovols',
     verifiedCreator: 'ovo1kT7RqrAZwFtgSGEgNfa7nHjeZoK6ykg1GknJEXG',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -168,6 +212,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Elixir: Ovols',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/elixir_ovols_pfp_1668964852701.png',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: true
     }
   },
@@ -177,6 +223,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Droid Capital',
     verifiedCreator: 'C88kKdEmTrCh43GC1w3BUFLsW3oKMEWWBGCyweQAaEYa',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -191,6 +238,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Droid Capital',
       marketImage:
         'https://i.seadn.io/gae/c5CkCKH28E2hKPnY2Tgj2H_maWZ06yqfeV4aQqkH00q1egB54S4BAc-6yCJsyHdQ716vp5amA1NPg-cyNzVjH1AR4HkBvimd_xq7Gw?auto=format&w=256',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -200,6 +249,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Vandal City',
     verifiedCreator: '8wACNDCJiPVVxfrFJRUYkJx4hQgvcoZggMXKmNvjQ6R7',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -214,6 +264,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Vandal City',
       marketImage:
         'https://i.seadn.io/gae/BgIPqBsvXBIoT-7IFHqAMbUJn9yU1Ngcdc5p1ItIQlderHXU5HuQX1SNWV4zqGpe_GWGcZwO88FFwpOdJMU7a5o4RMULdxmPr4TAlY4?auto=format&w=256',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -223,6 +275,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Heavenland',
     verifiedCreator: 'C4ki5erQ54EWDwuHYeD3m8xMrF6YXL9tV9dYpaf4idkS',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -237,6 +290,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Heavenland',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://dl.airtable.com/.attachmentThumbnails/9d1ed55dcb8a3f2a73d514c1e5fdc3e0/cecf9ba3',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -246,6 +301,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Marshies',
     verifiedCreator: 'ACBrgk19WYp6oT4VLHQZhAgDPJo8HNoCfDZW6iTpJSjS',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -260,6 +316,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Marshies',
       marketImage:
         'https://pbs.twimg.com/profile_images/1610792754388570113/LXrVtzAE_400x400.jpg',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -269,6 +327,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Drunken Ape Social Club',
     verifiedCreator: '56ruMizEJh4P12r6UqXEZ9AEYE9N5BtBortHfqp4m6Lm',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -283,6 +342,8 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Drunken Ape Social Club',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/drunkenapesc_pfp_1647995737060.jpeg',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   },
@@ -292,6 +353,7 @@ export const marketCollections: MarketTableRow[] = [
     name: 'Wolf Capital',
     verifiedCreator: 'DivyPfXM84oUP4LqW4YN2nNujHBRoggUDAKf9spppncD',
     rate: 0,
+    loanCurrency: 'SOL',
     available: 0,
     value: 0,
     allowance: 0,
@@ -306,9 +368,12 @@ export const marketCollections: MarketTableRow[] = [
       marketName: 'Wolf Capital',
       marketImage:
         'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://shdw-drive.genesysgo.net/2ZSU8JnjGSCjYgj9wTU4SRjTkz6kqv3WAQeGxgVh6yJG/19.png',
+      marketLoanCurrency: 'SOL',
+      marketLoanCurrencyImage: '/images/tokens/sol.svg',
       discountedMarket: false
     }
   }
+
   // {
   //   id: 'F8rZviSSuqgkTsjMeoyrTUSNSqh7yNDCAozJkxm7eujY',
   //   key: 'BURR',
@@ -429,6 +494,19 @@ export const renderMarketImageByID = (marketId: string) => {
     <Image
       src={`https://res.cloudinary.com/${cloudinary_uri}/image/fetch/${filteredMarket[0].constants.marketImage}`}
       alt={`${filteredMarket[0].constants.marketName} NFT image`}
+      layout="fill"
+    />
+  );
+};
+
+export const renderMarketCurrencyImageByID = (marketId: string) => {
+  const filteredMarket = marketCollections.filter(
+    market => market.constants.marketId === marketId
+  );
+  return (
+    <Image
+      src={filteredMarket[0].constants.marketLoanCurrencyImage}
+      alt={`${filteredMarket[0].constants.marketLoanCurrency} token image`}
       layout="fill"
     />
   );
