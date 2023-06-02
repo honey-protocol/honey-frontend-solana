274242d15c9bef75bf06c31a0fd48a8d1fc8cc89
--unset <bowtiedpyro@gmail.com>
2023-06-02 17:25:02 +0200

    wip borrow page speed optimisations

--- hooks/useFetchCollateralNFTPositions.tsx
+++ hooks/useFetchCollateralNFTPositions.tsx
@@ -36,19 +36,14 @@ export type PDA = { publicKey: PublicKey; bump: number };
 
 /**
  * @description fetches all collateralised positions of a user
- * @params honeyUser | wallet public key | current active market id
- * @returns array of collateral nft positions of user | isLoading: boolean
+ * @params connection | mint array: PublicKey[] | verified creator of the active market
+ * @returns Object containing: array of collateral nft positions of user, error, isloading boolean |  refreshPositions
  */
 export default function useFetchCollateralNFTPositions(
   connection: Connection,
   mintArray: PublicKey[],
   verified_creator: string
 ) {
-  // set state
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isRefetching, setIsRefetching] = useState(0);
-  // const [collateralNFTPositions, setCollateralNFTPositions] = useState<Array<CollateralNFTPosition>>([]);
-
   const [status, setStatus] = useState<{
     loading: boolean;
     collateralNFTPositions?: CollateralNFTPosition[];
@@ -56,70 +51,79 @@ export default function useFetchCollateralNFTPositions(
   }>({ loading: false });
 
   const fetchData = async () => {
-    setStatus({ loading: true });
+    try {
+      setStatus({ loading: true });
 
-    const collateralNFTPositions: CollateralNFTPosition[] = [];
-    const collateralNftMint: PublicKey[] = mintArray;
+      const collateralNFTPositions: CollateralNFTPosition[] = [];
+      const collateralNftMint: PublicKey[] = mintArray;
 
-    if (!collateralNftMint || collateralNftMint.length === 0) {
-      setStatus({
-        loading: false,
-        error: new Error('Obligation does not have a valid collateral nft mint')
-      });
-      return;
-    }
+      if (!collateralNftMint || collateralNftMint.length === 0) {
+        setStatus({
+          loading: false,
+          error: new Error(
+            'Obligation does not have a valid collateral nft mint'
+          )
+        });
+        return;
+      }
 
-    // map through all pubkeys and start process of fetching data
-    const promises = collateralNftMint.map(
-      async (key: PublicKey, index: number) => {
-        if (!key.equals(PublicKey.default)) {
-          const [nftMetadata, _] = await PublicKey.findProgramAddress(
-            [
-              Buffer.from('metadata'),
-              METADATA_PROGRAM_ID.toBuffer(),
-              key.toBuffer()
-            ],
-            METADATA_PROGRAM_ID
-          );
+      // map through all pubkeys and start process of fetching data
+      const promises = collateralNftMint.map(
+        async (key: PublicKey, index: number) => {
+          if (!key.equals(PublicKey.default)) {
+            const [nftMetadata, _] = await PublicKey.findProgramAddress(
+              [
+                Buffer.from('metadata'),
+                METADATA_PROGRAM_ID.toBuffer(),
+                key.toBuffer()
+              ],
+              METADATA_PROGRAM_ID
+            );
 
-          const data = await getNFTAssociatedMetadata(connection, nftMetadata);
-          if (!data) return;
+            const data = await getNFTAssociatedMetadata(
+              connection,
+              nftMetadata
+            );
+            if (!data) return;
 
-          const tokenMetadata = await Metadata.fromAccountAddress(
-            connection,
-            nftMetadata
-          );
+            const tokenMetadata = await Metadata.fromAccountAddress(
+              connection,
+              nftMetadata
+            );
 
-          // TODO: validate if we can run it or need to catch
-          // @ts-ignore
-          const verifiedCreator = tokenMetadata.data.creators.filter(
-            (creator: any) => creator.verified
-          )[0].address;
-          //   `(https://res.cloudinary.com/${cloudinary_uri}/image/fetch/${tokenMetadata.data.uri})`
+            // TODO: validate if we can run it or need to catch
+            // @ts-ignore
+            const verifiedCreator = tokenMetadata.data.creators.filter(
+              (creator: any) => creator.verified
+            )[0].address;
+            //   `(https://res.cloudinary.com/${cloudinary_uri}/image/fetch/${tokenMetadata.data.uri})`
 
-          // TODO: fetch via cloudinary
-          const arweaveData = await (
-            await fetch(tokenMetadata.data.uri)
-          ).json();
-          // only push if this obligation belows to current active market
-          if (verifiedCreator.toString() === verified_creator) {
-            collateralNFTPositions.push({
-              mint: nftMetadata,
-              updateAuthority: new PublicKey(tokenMetadata?.updateAuthority),
-              name: tokenMetadata?.data?.name,
-              symbol: tokenMetadata?.data?.symbol,
-              uri: tokenMetadata?.data?.uri,
-              image: arweaveData?.image,
-              verifiedCreator: verifiedCreator.toBase58()
-            });
+            // TODO: fetch via cloudinary
+            const arweaveData = await (
+              await fetch(tokenMetadata.data.uri)
+            ).json();
+            // only push if this obligation belows to current active market
+            if (verifiedCreator.toString() === verified_creator) {
+              collateralNFTPositions.push({
+                mint: nftMetadata,
+                updateAuthority: new PublicKey(tokenMetadata?.updateAuthority),
+                name: tokenMetadata?.data?.name,
+                symbol: tokenMetadata?.data?.symbol,
+                uri: tokenMetadata?.data?.uri,
+                image: arweaveData?.image,
+                verifiedCreator: verifiedCreator.toBase58()
+              });
+            }
           }
         }
-      }
-    );
+      );
 
-    await Promise.all(promises);
+      await Promise.all(promises);
 
-    return setStatus({ loading: false, collateralNFTPositions });
+      return setStatus({ loading: false, collateralNFTPositions });
+    } catch (error) {
+      console.log(`Error while fetching obligations: ${error}`);
+    }
   };
 
   const refreshPositions = async () => {
@@ -127,7 +131,6 @@ export default function useFetchCollateralNFTPositions(
   };
 
   useEffect(() => {
-    console.log('@@-- mint arr', mintArray);
     if (!mintArray) {
       setStatus({ loading: false, error: new Error('No mints found') });
       return;
--- hooks/useNFTV3.ts
+++ hooks/useNFTV3.ts
@@ -1,211 +1,3 @@
-// import {
-//   useState,
-//   useEffect,
-//   useRef,
-//   MutableRefObject,
-//   SetStateAction,
-//   Dispatch
-// } from 'react';
-// import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
-// import {
-//   resolveToWalletAddress,
-//   getParsedNftAccountsByOwner
-// } from '@nfteyez/sol-rayz';
-// import { ConnectedWallet, useSolana } from '@saberhq/use-solana';
-// import { programs } from '@metaplex/js';
-// import { MetadataKey } from '@nfteyez/sol-rayz/dist/config/metaplex';
-// import honeyGenesisBee from '/public/images/imagePlaceholder.png';
-
-// interface NFTArrayType {
-//   [index: string]: Array<NFT>;
-// }
-
-// const defaultNFT: NFT = {
-//   name: '',
-//   updateAuthority: '',
-//   image: '',
-//   creators: [],
-//   tokenId: '',
-//   mint: ''
-// };
-
-// export const defaultNFTImageUrl = honeyGenesisBee.src;
-// const defaultTokenId = 'defaultTokenId';
-
-// //this function should fetch all NFT from User
-// export default function useFetchCreatorNFTByUser(
-//   wallet: ConnectedWallet | null,
-//   creator?: string
-// ): [Array<NFT>, Boolean, Dispatch<SetStateAction<{}>>] {
-//   const [NFTs, setNFTs] = useState<Array<NFT>>([]);
-//   const [isLoading, setLoading] = useState(true);
-//   const providerMut = useSolana();
-//   //adding shouldRefetchNFTs to force refetching by passing {} to refecthNFTs
-//   const [shouldRefetchNFTs, refetchNFTs] = useState({});
-//   const shouldRefetchRef: MutableRefObject<{}> = useRef(shouldRefetchNFTs);
-//   const cache: MutableRefObject<NFTArrayType> = useRef({});
-//   const walletPublicKey = wallet?.publicKey?.toString() || '';
-
-//   useEffect(() => {
-//     let didCancel = false;
-//     const fetchNFTByUser = async () => {
-//       setLoading(true);
-//       if (!didCancel) {
-//         // const connection = new Connection(
-//         //   clusterApiUrl('mainnet-beta'),
-//         //   'processed'
-//         // );
-//         const connection = providerMut?.connection;
-//         if (walletPublicKey != '') {
-//           // we need to check if shouldRefetchNFTs trigger this function and we should do refetch instead of getting from cache
-//           if (
-//             cache.current[walletPublicKey] &&
-//             shouldRefetchRef.current === shouldRefetchNFTs
-//           ) {
-//             console.log(
-//               `fetching NFT for wallet public key ${walletPublicKey} from cache`
-//             );
-//             const result = cache.current[walletPublicKey];
-//             setNFTs(result);
-//           } else {
-//             console.log(
-//               `cache miss or force re-fetching, fetching NFT for wallet public key ${walletPublicKey}`
-//             );
-//             // check if wallet address is valid
-//             const publicAddress = await resolveToWalletAddress({
-//               text: walletPublicKey,
-//               connection
-//             });
-//             // get all NFT tokens from wallet || filter out all nfts that don't have creators array
-//             const nftArray = await getParsedNftAccountsByOwner({
-//               publicAddress,
-//               connection
-//             }).then(result => result.filter(nft => nft.data.creators));
-
-//             // We are mapping the exception to default NFT to be filtered out later to make typescript happy
-//             const results = nftArray.map(nft => ({
-//               name: nft.data.name,
-//               symbol: nft.data.name,
-//               updateAuthority: nft.updateAuthority,
-//               image: defaultNFTImageUrl,
-//               tokenUri: nft.data.uri,
-//               creators: nft.data.creators,
-//               mint: nft.mint,
-//               tokenId: nft.mint
-//             }));
-
-//             const validResults = results.filter(result => !(result.name == ''));
-//             cache.current[walletPublicKey] = validResults;
-//             shouldRefetchRef.current = shouldRefetchNFTs;
-//             setNFTs(validResults);
-//           }
-//         } else {
-//           setNFTs([]);
-//         }
-//       }
-//     };
-
-//     fetchNFTByUser()
-//       .catch(err => {
-//         console.error('Error calling fetch NFT by user');
-//         console.error(err);
-//         setNFTs([]);
-//       })
-//       .finally(() => {
-//         setLoading(false);
-//       });
-
-//     return () => {
-//       didCancel = true;
-//     };
-//   }, [wallet, shouldRefetchNFTs]);
-
-//   const fetchImageAndTokenIdForCreatorNFTs = async (
-//     nfts: NFT[],
-//     creator: string
-//   ) => {
-//     //filter out creator nfts and others from NFTs
-//     let creatorNFTs: NFT[] = [];
-//     let otherNFTs: NFT[] = [];
-//     for (let i = 0; i < nfts.length; i++) {
-//       if (nfts[i].creators.map(creator => creator.address).includes(creator)) {
-//         creatorNFTs.push(nfts[i]);
-//       } else {
-//         otherNFTs.push(nfts[i]);
-//       }
-//     }
-
-//     //if image url of first nft is default image url, fetch for all creator NFTs
-//     if (creatorNFTs.length && creatorNFTs[0].image === defaultNFTImageUrl) {
-//       //fetch image uri and tokenId for creatorNFts
-//       const promises = creatorNFTs.map(async nft => {
-//         const imageURI = await getNFTImgURI(nft.tokenUri ?? '');
-//         const tokenMetaPublicKey = await programs.metadata.Metadata.getPDA(
-//           new PublicKey(nft.mint)
-//         );
-//         const result: NFT = {
-//           name: nft.name,
-//           symbol: nft.name,
-//           updateAuthority: nft.updateAuthority,
-//           image: imageURI,
-//           creators: nft.creators,
-//           mint: nft.mint,
-//           tokenId: tokenMetaPublicKey.toString()
-//         };
-//         return result;
-//       });
-
-//       const results = await Promise.all(
-//         promises.map(p =>
-//           p.catch(e => {
-//             console.error('Error fetching individual NFT with error');
-//             console.error(e);
-//             return defaultNFT;
-//           })
-//         )
-//       );
-//       creatorNFTs = results;
-
-//       //update cache
-//       cache.current[walletPublicKey] = [...creatorNFTs, ...otherNFTs];
-//       //update state
-//       setNFTs([...creatorNFTs, ...otherNFTs]);
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (NFTs.length && creator) {
-//       fetchImageAndTokenIdForCreatorNFTs(NFTs, creator);
-//     }
-//   }, [creator, NFTs]);
-
-//   return [NFTs, isLoading, refetchNFTs];
-// }
-
-// //grab image URI from fetching from NFT uri
-// async function getNFTImgURI(uri: string) {
-//   if (uri && uri != '') {
-//     return fetch(uri)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error(
-//             'Network response was not OK when fetching NFT image URI'
-//           );
-//         }
-//         return response.json();
-//       })
-//       .then(result => {
-//         return result.image;
-//       })
-//       .catch(error => {
-//         console.error(error);
-//         return '';
-//       });
-//   } else {
-//     return '';
-//   }
-// }
-
 import {
   useState,
   useEffect,
@@ -246,10 +38,7 @@ export default function useFetchCreatorNFTByUser(
   const [NFTs, setNFTs] = useState<Array<NFT>>([]);
   const [isLoading, setLoading] = useState(true);
   const providerMut = useSolana();
-  //adding shouldRefetchNFTs to force refetching by passing {} to refecthNFTs
   const [shouldRefetchNFTs, refetchNFTs] = useState({});
-  // const shouldRefetchRef: MutableRefObject<{}> = useRef(shouldRefetchNFTs);
-  // const cache: MutableRefObject<NFTArrayType> = useRef({});
   const walletPublicKey = wallet?.publicKey?.toString() || '';
   const url = process.env.NEXT_PUBLIC_RPC_NODE;
 
@@ -259,16 +48,6 @@ export default function useFetchCreatorNFTByUser(
       setLoading(true);
       if (!didCancel) {
         if (walletPublicKey != '') {
-          // if (
-          //   cache.current[walletPublicKey] &&
-          //   shouldRefetchRef.current === shouldRefetchNFTs
-          // ) {
-          //   console.log(
-          //     `fetching NFT for wallet public key ${walletPublicKey} from cache`
-          //   );
-          //   const result = cache.current[walletPublicKey];
-          //   setNFTs(result);
-          // } else {
             console.log(
               `cache miss or force re-fetching, fetching NFT for wallet public key ${walletPublicKey}`
             );
@@ -316,8 +95,6 @@ export default function useFetchCreatorNFTByUser(
             );
 
             const validResults = outcome.filter((result: any) => !(result.name == ''));
-            // cache.current[walletPublicKey] = validResults;
-            // shouldRefetchRef.current = shouldRefetchNFTs;
             setNFTs(validResults);
           }
         } else {
