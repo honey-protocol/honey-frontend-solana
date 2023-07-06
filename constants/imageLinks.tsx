b4a6ceb170b4643d59b8f932e73dac577371e3cb
--unset <bowtiedpyro@gmail.com>
2023-07-06 16:17:19 +0200

    base setup links

---
+++ constants/imageLinks.tsx
@@ -0,0 +1,12 @@
+export const imageURLs = [
+  {
+    name: 'Honey Genesis Bee',
+    url: 'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://dl.airtable.com/.attachmentThumbnails/6b6c8954aed777a74de52fd70f8751ab/46b325db',
+    marketID: '6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3'
+  },
+  {
+    name: 'Mad Lads',
+    url: 'https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/mad_lads_pfp_1682211343777.png',
+    marketID: '2SC72EUqsd5Bax6u6vmmwcGiqjrjZrXJ7S1FsGfL4hBR'
+  }
+];
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,40 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { useRouter } from 'next/router';
+import { imageURLs } from 'constants/imageLinks';
+const cloudinary_uri = process.env.CLOUDINARY_URI;
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <meta
-          property="og:image"
-          content="honey-frontend-solana-git-market-og-image-honey-labs.vercel.app/api/og"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends NextDocument {
+  render() {
+    return (
+      <Html lang="eng">
+        <Head>
+          <DynamicMeta />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+const DynamicMeta = () => {
+  const marketId = '6FcJaAzQnuoA6o3sVw1GD6Ba69XuL5jinZpQTzJhd2R3';
+  const selectedImage = imageURLs.filter(
+    imageObject => imageObject.marketID === marketId
+  );
+
+  const dynamicImageUrl = selectedImage[0].url;
+
+  return (
+    <>
+      <meta
+        property="og:image"
+        content={`https://res.cloudinary.com/${cloudinary_uri}/image/fetch/${dynamicImageUrl}`}
+      />
+    </>
+  );
+};
+
+export default MyDocument;
